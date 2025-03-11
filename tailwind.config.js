0c05a3c5dbded986a60484992bba9f327064c1c6
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2025-03-11 20:00:58 +0530

    change config rule

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#3D405B",
+        heading: "#81B29A",
+        text: "#F2CC8F",
+      },
+    },
+  },
+  plugins: [],
+};
